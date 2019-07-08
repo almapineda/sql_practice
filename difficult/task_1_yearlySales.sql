e77cba185f5239e24d1546503847406df226120c
Alma Pineda <almapineda@berkeley.edu>
2019-07-08 10:10:24 -0700

    completed difficult task

--- difficult/task_1_yearlySales.sql
+++ difficult/task_1_yearlySales.sql
@@ -1,3 +1,5 @@
-	SELECT CAST(strftime('%Y', InvoiceDate) AS integer) AS YearOfSale, SUM(Total) AS TotalRev
-	FROM invoices
-	GROUP BY 1
+SELECT CAST(strftime('%Y', InvoiceDate) AS integer) AS YearOfSale,
+       SUM(Total) AS TotalRev,
+       SUM(TOTAL) / LAG(SUM(TOTAL)) OVER (ORDER BY MIN(InvoiceDate)) -1 AS PercentDifference
+FROM invoices
+GROUP BY 1
